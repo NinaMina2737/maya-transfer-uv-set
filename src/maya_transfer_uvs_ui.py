72d006b6209bdcbea52d3effbd67f2aa6132df70
NinaMina2737 <87418579+NinaMina2737@users.noreply.github.com>
2023-06-28 12:34:27 +0900

    Update maya_transfer_uvs_ui.py

--- src/maya_transfer_uvs_ui.py
+++ src/maya_transfer_uvs_ui.py
@@ -30,11 +30,63 @@ class TransferUVsUI(MayaQWidgetBaseMixin, QtWidgets.QWidget):
     def create_widget(self):
         """
         """
+        self.base_object_label = QtWidgets.QLabel("Select the base object:")
+        self.base_object_line_edit = QtWidgets.QLineEdit()
+        self.base_object_line_edit.setReadOnly(True)
+        self.base_object_line_edit.setText("None")
+
+        self.base_object_button = QtWidgets.QPushButton("Select Base Object")
+        self.base_object_button.clicked.connect(self.select_base_object)
+
+        self.execute_button = QtWidgets.QPushButton("Execute")
+        self.execute_button.clicked.connect(self.execute)
 
     def create_layout(self):
         """
         """
         main_layout = QtWidgets.QVBoxLayout()
+
+        main_layout.addWidget(self.base_object_label)
+        main_layout.addWidget(self.base_object_line_edit)
+        main_layout.addWidget(self.base_object_button)
+        main_layout.addWidget(self.execute_button)
+
+        self.setLayout(main_layout)
+
+    def select_base_object(self):
+        """
+        """
+        # Get the selected objects
+        objects = cmds.ls(selection=True)
+
+        # If no base object is specified, use the first selected object
+        if len(objects) > 0:
+            # Set the base object
+            self.base_object_line_edit.setText(objects[0])
+        else:
+            # Set the base object to None
+            self.base_object_line_edit.setText("None")
+
+    def execute(self):
+        """
+        """
+        if self.base_object_line_edit.text() == "None":
+            cmds.warning("No base object specified.")
+            return
+
+        # Get the base object
+        base_object = self.base_object_line_edit.text()
+
+        # Get the selected objects
+        objects = cmds.ls(selection=True)
+
+        # Check if any objects are selected
+        if len(objects) == 0:
+            cmds.warning("No objects selected.")
+            return
+
+        # Transfer the UVs
+        mtu.transfer_uvs(base_object=base_object, objects=objects)
 
 def execute():
     """
